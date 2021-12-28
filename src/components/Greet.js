3996d0aca44e46e0b9d2816e11fa426435255bec
Sikandar Waheed <ch.sikandar100@gmail.com>
2021-12-28 10:09:21 +0500

    Feat: First functional Component creation and usage (Video-5)

---
+++ src/components/Greet.js
@@ -0,0 +1,14 @@
+
+//ES5 syntax
+// function Greet() {
+//   return <h1>Hello Sikandar via Es5</h1>;
+// }
+
+//Es6 Syntax
+const Greet = () =><h1>Hello Sikandar via Es6</h1>
+
+
+//named Export (in this case you have to import with the same name and within curly braces while importing)
+//export const Greet = () =><h1>Hello Sikandar via Es6</h1>
+
+export default Greet;
