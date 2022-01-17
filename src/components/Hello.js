fba845398b3426522860ab461f299a8676c3d33f
Sikandar Waheed <ch.sikandar100@gmail.com>
2022-01-17 16:28:07 +0500

    Feat: classComponent without the JSX using createElement function of React Example code

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,9 +1,18 @@
+import React from "react";
+
 const Hello = () => {
-  //returning JSX in react
-  return (
-    <div>
-      <h1>Hello Sikandar</h1>
-    </div>
+    //returning JSX in react
+  //   return (
+  //     <div className='dummyClass'>
+  //       <h1>Hello Sikandar</h1>
+  //     </div>
+  //   );
+
+    //working without JSX using createElement
+  return React.createElement(
+    "div",
+    { id: "hello", className: "dummyClass" },
+    React.createElement("h1", null, "Hello Sikandar without JSX")
   );
 };
 
